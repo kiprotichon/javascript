45cb4fefb03540cb0836e8882a7f6c6616bff211
Tonny <kiprotichton9@gmail.com>
2025-07-09 12:08:41 +0300

    Add initial HTML, CSS, JavaScript files and variable declarations

---
+++ variables.js
@@ -0,0 +1,28 @@
+// variables are usedto store data that can be used throughout the applications .
+//They can be used to store configuration settings, user preferences ,or any other data
+//  that needs to be accesed globally
+
+//In js we can create variable using 'var' ,'let' or 'const' keywords
+
+//Keywords in programing languages are reserved words that have special meaning in the language.They are used to defune the structure and brehaviour of the code 
+// .In javasript keywords include 'var' , 'let , 'const' , 'funtion' , 'if' , 'else' , 'for , 'while'  and many more others.
+
+//Var was the original way to declare variables in javascrit, Because of scope issues it is now recomended to use let and const instead
+var username ="TONNY"
+
+//Let is used to declare variables that can be reassigned later .It has b;lock sco[pe, meaning it is only assecible within the block it is derfined in
+let age=30;
+
+//const is used to declare variables that cannot be reassigned.It also block scoped
+const pi =3.14;
+const role = "distributor"
+let darkmode =false;
+//let variables can be reassigned ,but const variables cannot be reassigned.
+age = 31;
+//pi = 3.2;
+
+
+
+
+
+
